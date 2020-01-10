f86215c3189b92c7718f11058115b23935092fde
Ali Sayed <alisayed@Alis-Mac-mini.local>
2020-01-10 13:01:50 -0500

    switched to a different news api, still missing implementation of a few parameters

--- src/fetch_news.js
+++ src/fetch_news.js
@@ -1,43 +1,65 @@
-const endPoint = "https://api.currentsapi.services";
-const latestNews = "/v1/latest-news?";
-const searchNews = "/v1/search?";
+const endPoint = "https://newsapi.org";
+const topNews = "/v2/top-headlines?";
+const searchNews = "/v2/everything?";
+const sourcesFinder = "/v2/sources?";
 const languageSelector = "language=";
-const keywordsSelector = "keywords=";
+const keywordsSelector = "q=";
 const countrySelector = "country=";
 const categorySelector = "category=";
-const domainSelector = "domain=";
-const apiKey = "apiKey=MTLbAdBXovp6xN8F8tpR9lOWcbvIhzMb0xpy1ei2TSKNGEcx";
+const sourcesSelector = "sources=";
+const apiKey = "apiKey=5024963fa5464b67b100d3613c415c02";
 const defaultLanguage = "en";
 
 export async function fetchNews(typeOfNews, options) {
   let submitPoint = endPoint;
 
-  if (typeOfNews === "latest") {
-    submitPoint += latestNews;
+  // Update submit point url depending on type of news requested //
+  if (typeOfNews === "top") {
+    submitPoint += topNews;
   } else if (typeOfNews === "search") {
     submitPoint += searchNews;
   }
 
+  // Add default english language parameter if no language specified //
   if (options && options.language) {
     submitPoint += `${languageSelector}${options.language}`;
   } else {
     submitPoint += `${languageSelector}${defaultLanguage}`;
   }
 
-  if (options && typeOfNews === "search") {
+  // Add parameters that can be added to a top news request only//
+  if (options && typeOfNews === "top") {
     if ("country" in options)
       submitPoint += `&${countrySelector}${options.country}`;
+  }
+
+  // Add parameters that can be added to a search news request only //
+  if (options && typeOfNews === "search") {
+  }
+
+  // Add parameters that can be added to both types of news requests request only //
+  if (options) {
     if ("keywords" in options)
       submitPoint += `&${keywordsSelector}${options.keywords}`;
-    if ("domain" in options)
-      submitPoint += `&${domainSelector}${options.domain}`;
+    if ("sources" in options)
+      submitPoint += `&${sourcesSelector}${options.sources}`;
     if ("category" in options)
       submitPoint += `&${categorySelector}${options.category}`;
   }
 
+  // Add api key //
   submitPoint += `&${apiKey}`;
+
   console.log(submitPoint);
+
+  // Fetch news //
   const results = await fetch(submitPoint);
   const news = await results.json();
-  // console.log(news);
+  console.log(news);
+}
+
+export async function fetchSources() {
+  const results = await fetch(`${endPoint}${sourcesFinder}${apiKey}`);
+  const sources = await results.json();
+  console.log(sources);
 }
--- src/script.js
+++ src/script.js
@@ -1,6 +1,14 @@
 // Imports //
-import { fetchNews } from "./fetch_news";
+import { fetchNews, fetchSources } from "./fetch_news";
 
 console.log("it works");
 
-fetchNews("latest");
+//! Get news //
+// fetchNews("search", {
+//   country: "gb",
+//   sources: "al-jazeera-english",
+//   keywords: "iran"
+// });
+
+//! Get sources //
+// fetchSources();
